7b8a2b1c25b79e015f1baff9ac9c3cfca67c7f52
João Gonçalves <joao.n.d.goncalves@gmail.com>
2022-12-02 11:49:44 +0000

    login form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
 import Login from './components/login/Login';
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
